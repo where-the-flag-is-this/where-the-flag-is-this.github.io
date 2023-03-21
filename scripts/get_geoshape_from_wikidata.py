aab23680f8d7bd292ab9b8deb24e4009418a82ae
jellevankerk <jelle.95@live.nl>
2023-03-21 20:31:23 +0100

    update script

--- scripts/get_geoshape_from_wikidata.py
+++ scripts/get_geoshape_from_wikidata.py
@@ -4,14 +4,24 @@ import json
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-url = 'https://commons.wikimedia.org/w/api.php?action=query&prop=revisions&rvslots=*&rvprop=content&format=json&titles=Data:Germany.map&origin=*'
 
-response = requests.get(url)
+def get_geoshape(name_geoshape:str):
+    url = f'https://commons.wikimedia.org/w/api.php?action=query&prop=revisions&rvslots=*&rvprop=content&format=json&titles=Data:{name_geoshape}.map&origin=*'
+    
 
-data = response.json()['query']['pages']
-geoshape = json.loads(data[list(data.keys())[0]]['revisions'][0]['slots']['main']['*'])['data']
+    response = requests.get(url)
 
-gdf = gpd.GeoDataFrame.from_features(geoshape["features"])
+    data = response.json()['query']['pages']
+    geoshape = json.loads(data[list(data.keys())[0]]['revisions'][0]['slots']['main']['*'])['data']
+
+    return geoshape["features"][0]
+
+countries = ['Germany','Italy', 'Belgium','Poland' ]
+geoshapes = [get_geoshape(x) for x in countries]
+geoshapes_dict = {'type': 'FeatureCollection', }
+geoshapes_dict['features'] = geoshapes
+
+gdf = gpd.GeoDataFrame.from_features(geoshapes_dict)
 
 gdf.plot()
 plt.show()
