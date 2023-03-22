89c481208654db44148de15a2e30b7659b221236
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-22 08:18:55 +0100

    add base CRS
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import { ref } from "vue";
-import customCRS from "./customCRS";
+import { CRS } from "leaflet";
+
 
 const zoom = ref(2)
 const map = ref();
-const crs = customCRS;
+const crs = CRS.Base;
+
 function initMap(mapObj) {
   map.value = mapObj;
 }
 </script>
 
 <template>
-  <div style="height:600px; width:800px">
-    <l-map id="root" @ready="initMap" :crs="crs" class="rounded-b-lg" v-model="zoom" v-model:zoom="zoom"
-      :center="[47.41322, -1.219482]" :options="{ attributionControl: false }" :min-zoom="1" :max-zoom="18">
+  <div class="w-screen h-screen bg-green-400">
+    <l-map id="root" @ready="initMap" :crs="crs" v-model="zoom" v-model:zoom="zoom" :center="[0, 0]"
+      :options="{ attributionControl: false }" :min-zoom="1" :max-zoom="18">
       <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
         name="OpenStreetMap"></l-tile-layer>
     </l-map>
