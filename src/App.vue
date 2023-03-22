4ce9457f49e3259d2412b06b4ba014c2bbe21369
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-22 16:06:00 +0100

    move marker by clicking
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 import { ref } from "vue";
-import { CRS } from "leaflet";
-
+import { CRS, latLng } from "leaflet";
 
 const zoom = ref(2)
 const map = ref();
 const crs = CRS.Base;
-
+const markerPosition = ref(latLng(0, 0))
 function initMap(mapObj) {
   map.value = mapObj;
+  map.value.doubleClickZoom.disable();
+  map.value.addEventListener('click', function (ev) {
+    markerPosition.value = latLng(ev.latlng.lat, ev.latlng.lng)
+  });
 }
+
 </script>
 
 <template>
   <div class="w-screen h-screen bg-green-400">
     <l-map id="root" @ready="initMap" :crs="crs" v-model="zoom" v-model:zoom="zoom" :center="[0, 0]"
       :options="{ attributionControl: false }" :min-zoom="1" :max-zoom="18">
+      <l-marker :lat-lng="markerPosition" />
       <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
         name="OpenStreetMap"></l-tile-layer>
     </l-map>
