ef1aac40c662079e38c681859aa76689f631e240
jellevankerk <jelle.95@live.nl>
2023-03-22 08:00:18 +0100

    add query to play with

--- scripts/get_geoshape_from_wikidata.py
+++ scripts/get_geoshape_from_wikidata.py
@@ -1,9 +1,11 @@
 import requests
 import json 
+import random
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
+from queries import WikiDataQueryResults, countries_information_query
+random.seed(20)
 
 def get_geoshape(name_geoshape:str):
     url = f'https://commons.wikimedia.org/w/api.php?action=query&prop=revisions&rvslots=*&rvprop=content&format=json&titles=Data:{name_geoshape}.map&origin=*'
@@ -16,12 +18,14 @@ def get_geoshape(name_geoshape:str):
 
     return geoshape["features"][0]
 
-countries = ['Germany','Italy', 'Belgium','Poland' ]
-geoshapes = [get_geoshape(x) for x in countries]
+query = WikiDataQueryResults(countries_information_query)
+
+country_list = query.load_as_list('countryLabel') 
+geoshapes = [get_geoshape(x) for x in random.sample(country_list, 10)]
 geoshapes_dict = {'type': 'FeatureCollection', }
 geoshapes_dict['features'] = geoshapes
 
 gdf = gpd.GeoDataFrame.from_features(geoshapes_dict)
 
-gdf.plot()
+gdf.boundary.plot()
 plt.show()
---
+++ scripts/queries.py
@@ -0,0 +1,87 @@
+import sys
+import pandas as pd
+from typing import List, Dict
+from pprint import pprint
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+class WikiDataQueryResults:
+    """
+    A class that can be used to query data from Wikidata using SPARQL and return the results as a Pandas DataFrame or a list
+    of values for a specific key.
+    """
+    def __init__(self, query: str):
+        """
+        Initializes the WikiDataQueryResults object with a SPARQL query string.
+
+        :param query: A SPARQL query string.
+        """
+        self.user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+        self.endpoint_url = "https://query.wikidata.org/sparql"
+        self.sparql = SPARQLWrapper(self.endpoint_url, agent=self.user_agent)
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+
+    def __transform2dicts(self, results: List[Dict]) -> List[Dict]:
+        """
+        Helper function to transform SPARQL query results into a list of dictionaries.
+
+        :param results: A list of query results returned by SPARQLWrapper.
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
+        new_results = []
+        for result in results:
+            new_result = {}
+            for key in result:
+                new_result[key] = result[key]['value']
+            new_results.append(new_result)
+        return new_results
+
+    def load_as_dataframe(self) -> pd.DataFrame:
+        """
+        Executes the SPARQL query and returns the results as a Pandas DataFrame.
+
+        :return: A Pandas DataFrame representing the query results.
+        """
+        results = self._load()
+        return pd.DataFrame.from_dict(results)
+
+    def load_as_list(self, key: str) -> List[str]:
+        """
+        Executes the SPARQL query and returns a list of values for a specific key.
+
+        :param key: The key for which to retrieve values.
+        :return: A list of string values for the specified key.
+        """
+        results = self._load()
+        return [x[key] for x in results]
+
+    def _load(self) -> List[Dict]:
+        """
+        Helper function that loads the data from Wikidata using the SPARQLWrapper library, and transforms the results into
+        a list of dictionaries.
+
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
+        results = self.sparql.queryAndConvert()['results']['bindings']
+        results = self.__transform2dicts(results)
+        return results
+
+countries_information_query = """
+SELECT ?country ?countryLabel ?countryDescription ?flag ?location ?population ?area ?geoshape 
+WHERE 
+{
+  ?country wdt:P31 wd:Q3624078. # select items with "country" classification
+  FILTER NOT EXISTS {?country wdt:P31/wdt:P279* wd:Q1246}. # filter out recognized countries
+  FILTER NOT EXISTS {?country wdt:P576 ?dissolved.} # filter out items with date of dissolution
+  FILTER (?country != wd:Q219060) # exclude Taiwan
+  FILTER (?country != wd:Q865) # exclude Palestine
+  OPTIONAL { ?country wdt:P41 ?flag. } # get flag of the country, if any
+  OPTIONAL { ?country wdt:P625 ?location. } # get location of the country, if any
+  OPTIONAL { ?country wdt:P1082 ?population. } # get population of the country, if any
+  OPTIONAL { ?country wdt:P2046 ?area. } # get area of the country, if any
+  OPTIONAL { ?country wdt:P3896 ?geoshape. } # get geoshape of the country, if any
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". } # get label and description in English 
+}
+"""
