8d515900528fed3abb515d622c1ef46810ef27b9
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-23 10:41:50 +0100

    add sidebar
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,17 @@ function initMap(mapObj) {
 </script>
 
 <template>
-  <div class="w-screen h-screen bg-green-400">
-    <l-map id="root" @ready="initMap" :crs="crs" v-model="zoom" v-model:zoom="zoom" :center="[0, 0]"
-      :options="{ attributionControl: false }" :min-zoom="1" :max-zoom="18">
-      <l-marker :lat-lng="markerPosition" />
-      <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
-        name="OpenStreetMap"></l-tile-layer>
-    </l-map>
+  <div class="w-screen h-screen bg-green-400 flex">
+    <div class="w-4/5 h-full overflow-x-hidden">
+      <l-map id="root" @ready="initMap" :crs="crs" v-model="zoom" v-model:zoom="zoom" :center="[0, 0]"
+        :options="{ attributionControl: false }" :min-zoom="1" :max-zoom="18">
+        <l-marker :lat-lng="markerPosition" />
+        <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+          name="OpenStreetMap"></l-tile-layer>
+      </l-map>
+    </div>
+    <div class="w-1/5 h-full bg-slate-200 overflow-x-hidden">
+
+    </div>
   </div>
 </template>
