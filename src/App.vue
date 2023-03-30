706d1e2309d4ede6a442931c732764959e87d2bf
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-30 13:51:05 +0200

    split mapView in own component
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <script setup lang="ts">
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "@vue-leaflet/vue-leaflet";
 import { ref, computed } from "vue";
-import { CRS, latLng } from "leaflet";
 
 import { storeToRefs } from 'pinia';
 import { useGameStateStore } from './stores/gameState';
+import MapView from "./components/mapView.vue"
 
-const { places } = storeToRefs(useGameStateStore());
-
-const allPlaces = places
-const shuffleAllPlaces = () => {
-  let _newFeatures = allPlaces.value
-  _newFeatures.sort(() => Math.random() - 0.5)
-  allPlaces.value = _newFeatures
-}
+const gameStateStore = useGameStateStore()
+const { places, currentIndex, gameState, markerPosition } = storeToRefs(gameStateStore);
+const { shuffleAllPlaces } = gameStateStore
 
 shuffleAllPlaces()
-const gameState = ref<"none" | "won" | "lose" | "correctRound" | "ongoingRound">("none")
-const map = ref();
-const crs = CRS.Base;
-const markerPosition = ref(latLng(0, 0))
-const currentIndex = ref(0)
-const placePolygon = ref(null)
-
-function initMap(mapObj) {
-  map.value = mapObj;
-  map.value.doubleClickZoom.disable();
-  map.value.addEventListener('click', function (ev) {
-    markerPosition.value = latLng(ev.latlng.lat, ev.latlng.lng)
-  });
-}
 
 const currentPlace = computed(() => places.value[currentIndex.value])
 
@@ -66,7 +46,7 @@ const guess = () => {
   if (gameState.value === "ongoingRound") {
     const isCorrect = isMarkerInsidePolygon()
     if (isCorrect) {
-      if (currentIndex.value == (allPlaces.value.length - 1)) {
+      if (currentIndex.value == (places.value.length - 1)) {
         gameState.value = "won"
       } else {
         gameState.value = "correctRound"
@@ -105,32 +85,12 @@ const score = computed(() => {
 
 })
 
-const polyVisible = computed(() => {
-  if (["won", "lose", "correctRound"].indexOf(gameState.value) > -1)
-    return true
-  return false
-})
-const polyColor = computed(() => {
-  if (["won", "correctRound"].indexOf(gameState.value) > -1) {
-    return "#00FF00"
-  } else if (["lose"].indexOf(gameState.value) > -1) {
-    return "#FF0000"
-  }
-  return "#0000FF"
-})
 </script>
 
 <template>
   <div class="w-screen h-screen bg-green-400 flex">
     <div class="w-4/5 h-full overflow-x-hidden">
-      <l-map id="root" @ready="initMap" :crs="crs" :zoom="2" :center="[0, 0]" :options="{ attributionControl: false }"
-        :min-zoom="1" :max-zoom="18">
-        <l-marker :lat-lng="markerPosition" />
-        <l-tile-layer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap"
-          :no-wrap="true" />
-        <l-geo-json ref="placePolygon" :geojson="currentPlace" :visible="polyVisible"
-          :optionsStyle="() => { return { 'color': polyColor, 'fillColor': polyColor } }" />
-      </l-map>
+      <MapView />
     </div>
     <div class="w-1/5 h-full bg-slate-200 overflow-x-hidden">
       <div class="flex flex-col justify-between h-full">
--- src/stores/gameState.ts
+++ src/stores/gameState.ts
@@ -2,12 +2,21 @@ import { defineStore } from "pinia";
 import { ref, Ref } from "vue";
 import { Place } from "../types/Place";
 import allPlaces from "../assets/allPlaces.json"
+import { LatLng, latLng } from "leaflet";
 
 export const useGameStateStore = defineStore('gameState', () => {
     const places: Ref<Place[]> = ref(allPlaces.features); // ref = state
     const currentIndex: Ref<number> = ref(0)
+    const gameState: Ref<"none" | "won" | "lose" | "correctRound" | "ongoingRound"> = ref("none")
+    const markerPosition: Ref<LatLng> = ref(latLng(0, 0))
+
+    const shuffleAllPlaces = () => {
+        let _newFeatures = places.value
+        _newFeatures.sort(() => Math.random() - 0.5)
+        places.value = _newFeatures
+    }
 
     return {
-        places, currentIndex
+        places, currentIndex, gameState, shuffleAllPlaces, markerPosition
     }
 })
